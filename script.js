ef04c68df3b03497207f5b48c6cf39ebc50a4c2e
RizkiRezaAlGifari <reza10222104@sttcipasung.ac.id>
2024-11-23 16:02:54 +0700

    todo-app-KEL1
    test masih loba nu salah

---
+++ script.js
@@ -0,0 +1,4 @@
+const toDoInput; = document.getElementById('toDoInput')
+const toDoBtn; = document.getElementById('toDoBtn')
+const toDoAlert; = document.getElementById('toDoAlert')
+const toDoListItem; = document.getElementById('toDoListItem')
